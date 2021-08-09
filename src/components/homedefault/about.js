0aeb63e6be3d0e486f760aec931dc60b5da4d171
eddie <edgardojosemedina2@gmail.com>
2021-08-08 20:46:12 -0400

    moved about section to graphCMS

--- src/components/homedefault/about.js
+++ src/components/homedefault/about.js
@@ -33,8 +33,9 @@ const About = ( ) => {
   const email = data.allGraphCmsAbout.edges[0].node.email;
   const phone = data.allGraphCmsAbout.edges[0].node.phone;
   const age = data.allGraphCmsAbout.edges[0].node.age;
-  const resume = data.allGraphCmsAbout.edges[0].node.resume.url;
 
+  const resume = data.allGraphCmsAbout.edges[0].node.resume.url;
+  
   const infoList = [
   
     {
@@ -104,7 +105,7 @@ console.log(data)
                                     </ul>
                                 }
                                 <div className="button-group mt--20">
-                                   <a className="rn-button"  target="_blank" href={resume}><span>resume</span></a>
+                                   <a className="rn-button" href={resume}><span>resume</span></a>
                                    
                                 </div>
                             </div>
